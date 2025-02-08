7f0b4639e7c991c9393e44490fa1c1ed5375b8c0
Erik Rapa <erik.rapa@visma.com>
2025-02-08 12:47:58 +0200

    Integrate Identity

---
+++ FIF.Persistence/Extensions/MigrationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FIF.Persistence.Extensions
+{
+    public static class MigrationExtensions
+    {
+        public static void ApplyMigrations(this IApplicationBuilder app)
+        {
+            using var serviceScope = app.ApplicationServices.CreateScope();
+            var dbContext = serviceScope.ServiceProvider.GetService<AppDbContext>()!;
+            dbContext.Database.Migrate();
+        }
+    }
+}

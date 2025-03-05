e4a50e3635cd9ca1dea80f2c05f9fb801b853c04
Erik Rapa <erik.rapa@visma.com>
2025-03-05 21:25:10 +0200

    Fixing by the result of review

--- FIF.Registration/Models/PersonRegistrationModel.cs
+++ FIF.Registration/Models/PersonRegistrationModel.cs
@@ -4,20 +4,20 @@ namespace FIF.Registration.Models
 {
     public class PersonRegistrationModel
     {
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }// TODO: we should remove this field
-        public string Phone { get; set; }
-        public string PersonalIdentificationNumber { get; set; } //CNP
-        public Sex Sex { get; set; }
-        public DateTime BirthDate { get; set; }
-        public TShrtSize TShrtSize { get; set; }
-        public MealPreferences MealPreferences { get; set; }
-        public Religion Religion { get; set; }
-        public string Country { get; set; }
-        public string County { get; set; }
-        public string Location { get; set; }
-        public string Street { get; set; }
-        public string HouseNumber { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }// TODO: we should remove this field
+        public required string Phone { get; set; }
+        public required string PersonalIdentificationNumber { get; set; } //CNP
+        public required Sex Sex { get; set; }
+        public required DateTime BirthDate { get; set; }
+        public required TShrtSize TShrtSize { get; set; }
+        public required MealPreferences MealPreferences { get; set; }
+        public required Religion Religion { get; set; }
+        public required string Country { get; set; }
+        public required string County { get; set; }
+        public required string Location { get; set; }
+        public required string Street { get; set; }
+        public required string HouseNumber { get; set; }
     }
 }
--- FIF.Registration/Validators/PersonRegistrationModelValidator.cs
+++ FIF.Registration/Validators/PersonRegistrationModelValidator.cs
@@ -7,18 +7,18 @@ namespace FIF.Registration.Validators
     {
         public PersonRegistrationModelValidator()
         {
-            RuleFor(c => c.FirstName).NotNull().NotEmpty().WithMessage("First name is required");
-            RuleFor(c => c.LastName).NotNull().NotEmpty().WithMessage("Last name is required");
+            RuleFor(c => c.FirstName).NotEmpty().WithMessage("First name is required");
+            RuleFor(c => c.LastName).NotEmpty().WithMessage("Last name is required");
 
             RuleFor(c => c.Phone)
-                .NotNull()
+                
                 .NotEmpty()
                 .WithMessage("Phone is required")
                 .Matches(@"^\+40[1-9][0-9]{8,9}$|^\+36[1-9][0-9]{8}$|^\d{10}$")
                 .WithMessage("Phone number is not valid");
 
             RuleFor(c => c.PersonalIdentificationNumber)
-                .NotNull()
+                
                 .NotEmpty()
                 .WithMessage("Personal Identification Number is required")
                 .Length(13)
@@ -27,22 +27,22 @@ namespace FIF.Registration.Validators
                 .WithMessage("Personal Identification Number must be numeric and exactly 13 digits long");
 
             RuleFor(c => c.Sex).IsInEnum().WithMessage("Sex is required");
-            RuleFor(c => c.BirthDate).NotNull().NotEmpty().WithMessage("Birth date is required");
+            RuleFor(c => c.BirthDate).NotEmpty().WithMessage("Birth date is required");
             RuleFor(c => c.TShrtSize).IsInEnum().WithMessage("T-shirt size is required");
             RuleFor(c => c.MealPreferences).IsInEnum().WithMessage("Meal preferences are required");
             RuleFor(c => c.Religion).IsInEnum().WithMessage("Religion is required");
 
             RuleFor(c => c.Country)
-                 .NotNull()
+                 
                 .NotEmpty()
                 .WithMessage("Country is required")
                 .Must(country => AllowedCountries.Contains(country))
                 .WithMessage($"Country must be one of the predefined countries {string.Join(", ", AllowedCountries)}");
 
-            RuleFor(c => c.County).NotNull().NotEmpty().WithMessage("County is required");
-            RuleFor(c => c.Location).NotNull().NotEmpty().WithMessage("Location is required");
-            RuleFor(c => c.Street).NotNull().NotEmpty().WithMessage("Street is required");
-            RuleFor(c => c.HouseNumber).NotNull().NotEmpty().WithMessage("House number is required");
+            RuleFor(c => c.County).NotEmpty().WithMessage("County is required");
+            RuleFor(c => c.Location).NotEmpty().WithMessage("Location is required");
+            RuleFor(c => c.Street).NotEmpty().WithMessage("Street is required");
+            RuleFor(c => c.HouseNumber).NotEmpty().WithMessage("House number is required");
         }
 
         private static readonly List<string> AllowedCountries = new List<string>
